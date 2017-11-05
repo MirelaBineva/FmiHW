a6122be01c3ca1f5e9ae9173650778f0b1b716f5
MirelaBineva <33381175+MirelaBineva@users.noreply.github.com>
2017-11-05 14:54:47 +0200

    Add files via upload

---
+++ hw_zad1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //zad 1
+    int n;
+    cout << "n = ";
+    cin >> n;
+    int sum = 0;
+    int userNumber;
+    for (int i=0; i<n; i++)
+    {
+        do
+        {
+            cout << "Input: ";
+            cin >> userNumber;
+        }while (userNumber < 10 || userNumber > 5555);
+
+        sum +=userName;
+        cout << "Current sum : " << sum << endl;
+
+    }
+
+
+    return 0;
+}
